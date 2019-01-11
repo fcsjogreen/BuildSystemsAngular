cfc179f8a8bb6cfcd21c3d4db9b24deca2874a52
fcsjogreen <fiocsj@gmail.com>
2019-01-10 20:02:56 -0500

    build
    algunos cambios para produccion

--- bodega/src/environments/environment.prod.ts
+++ bodega/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCZspbXsxdvAZEjYnUxqLoWves0IeuPeFU",
+    authDomain: "bodega-e4925.firebaseapp.com",
+    databaseURL: "https://bodega-e4925.firebaseio.com",
+    projectId: "bodega-e4925",
+    storageBucket: "bodega-e4925.appspot.com",
+    messagingSenderId: "197587334934"
+  }
 };
